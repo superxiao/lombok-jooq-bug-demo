ec2106300afffc6aca6643ca140fb0018e83e571
Xiaojian Wang <xiaojianw@saharalabs.ai>
2024-11-17 22:49:08 +0800

    Move generated code to src

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   java
-  id("dev.monosoul.jooq-docker") version "6.1.11"
   id("org.springframework.boot") version "3.3.5"
 }
 
@@ -9,7 +8,6 @@ repositories {
 }
 
 dependencies {
-  jooqCodegen("org.postgresql:postgresql:42.7.4")
   implementation("org.springframework.boot:spring-boot-starter-web:3.3.5")
   implementation("org.jooq:jooq:3.19.15")
   compileOnly("org.projectlombok:lombok:1.18.36")
