f4cf067c6573e7507ae20729134622d934546fcc
Xiaojian Wang <xiaojianw@saharalabs.ai>
2024-11-17 22:44:52 +0800

    Minor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
   java
-  id("io.freefair.lombok") version "8.10.2"
   id("dev.monosoul.jooq-docker") version "6.1.11"
-
   id("org.springframework.boot") version "3.3.5"
 }
 
@@ -14,8 +12,12 @@ dependencies {
   jooqCodegen("org.postgresql:postgresql:42.7.4")
   implementation("org.springframework.boot:spring-boot-starter-web:3.3.5")
   implementation("org.jooq:jooq:3.19.15")
+  compileOnly("org.projectlombok:lombok:1.18.36")
+  annotationProcessor("org.projectlombok:lombok:1.18.36")
 
   implementation("org.jetbrains:annotations:26.0.1")
+
+
 }
 
 group = "com.example"
